fe91f2e550d6d93d4353bef63bc61eeea8f6a9bf
Leila <Leila@DESKTOP-LU1NCHG>
2019-12-11 17:06:03 +0100

    Figuras1_1

---
+++ Figuras1_1/src/figuras1_1/Figuras1_1.java
@@ -0,0 +1,19 @@
+package figuras1_1;
+
+/**
+ *
+ * @author Leila
+ */
+public class Figuras1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Triangulo obj=new Triangulo();
+        obj.Triangulo();
+        
+        
+    }
+    
+}
---
+++ Figuras1_1/src/figuras1_1/Triangulo.java
@@ -0,0 +1,25 @@
+package figuras1_1;
+
+/**
+ *
+ * @author Leila
+ */
+public class Triangulo {
+
+    public void Triangulo(){
+        int max=20;
+        int base=0,altura=0;
+ 
+        for(base=1;base<=max;base=base+1)
+        {
+           
+            for(altura=0;altura<base;altura++)
+            {
+                System.out.print("a");
+            }
+            System.out.println();
+        }
+    }
+        
+    
+}
